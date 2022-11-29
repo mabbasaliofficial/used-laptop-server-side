129502bc82b3ad94b4ede36dd19e61543ebf8477
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-11-29 23:06:27 +0600

    delete operation added

--- index.js
+++ index.js
@@ -104,6 +104,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+    });
+
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
